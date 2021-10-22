7615b3b0f2f40af3fbb21d6d67eb9c7306f574ab
AllyOfZendikar <jaimecabparr@gmail.com>
2021-10-22 16:04:05 -0400

    Instalando la dependencia gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const { src, dest, watch } = require( 'gulp' );
 const sass = require( 'gulp-sass' )( require( 'sass' ) ); 
-
+const plumber = require( 'gulp-plumber' );
 
 function css( donde ) {
 
     src('src/scss/**/*.scss')            // Identificar el archivo .scss a compilar
+        .pipe( plumber() )              // Plumber evita que se detenga el workflow cuando se encuentra un error
         .pipe( sass() )                 // Compilarlo
         .pipe( dest( 'build/css' ) )    // Almacenarla en el disco duro
 
